fede4dd33d315b0ba3b4416f287dde808cfb86d9
maheshudvag <maheshudvag@gmail.com>
2024-04-15 18:32:08 +0530

    Added predefined ranges as prop

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,10 @@ function App() {
   return (
     <>
       <h1>Date Picker</h1>
+      <h4>Date Picker with all options</h4>
       <DatePickerInput getSelectedDates={getSelectedDates} preDefinedRanges={Object.values(DATE_RANGE)}/>
+      <h4>Date Picker with Fewer options</h4>
+      <DatePickerInput getSelectedDates={getSelectedDates} preDefinedRanges={[DATE_RANGE.YESTERDAY, DATE_RANGE.LAST_30_DAYS,DATE_RANGE.LAST_7_DAYS,DATE_RANGE.THIS_MONTH]}/>
     </>
   )
 }
